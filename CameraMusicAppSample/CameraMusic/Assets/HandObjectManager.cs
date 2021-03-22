772306136b886508d0011cb316b09317dffef783
smpik <32724178+smpik@users.noreply.github.com>
2021-03-22 23:03:10 +0900

    カメラ映像から手検出するプロジェクトを登録

---
+++ CameraMusicAppSample/CameraMusic/Assets/HandObjectManager.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandObjectManager : MonoBehaviour
+{
+	public GameObject handObjectPrefab;
+
+	private const float ERROR_MARGIN = 1;// 移動したと判定するかの許容誤差
+
+	private List<Vector3> detectedHandPositions = new List<Vector3>();
+	private List<Vector3> generatedHandPositions = new List<Vector3>();
+	private List<GameObject> handObjects = new List<GameObject>();
+
+	/*
+	 * 初期化処理
+	 */
+	private void Start()
+	{
+		/* 値の初期化	*/
+		detectedHandPositions.Clear();
+		generatedHandPositions.Clear();
+		handObjects.Clear();
+	}
+
+	/*
+	 * 手検出開始通知
+	 */
+	public void NotifyStartDetectHand()
+	{
+		detectedHandPositions.Clear();
+	}
+
+	/*
+	 * 手検出通知
+	 */
+	public void NotifyDetectedHand(int handId, int posX, int posY)
+	{
+		/* 座標変換処理(retMatの1280x720⇒world座標)	*/
+		float convertedPosX = (posX * (1000.0f / 1280)) - 500;// Planeの左端のx座標が-500、右端のx座標が500なので。
+		float convertedPosY = -(posY * (1000.0f / 720)) + 500;// Planeの下端のx座標が-500、上端のx座標が500なので。
+
+		/* 検出した座標をストックする	*/
+		detectedHandPositions.Add(new Vector3(convertedPosX, convertedPosY, 0));
+	}
+
+	/*
+	 * 手検出終了通知
+	 */
+	public void NotifyEndDetectHand()
+	{
+		ManageHandObject();
+	}
+
+	/*
+	 * HandObject管理処理
+	 */
+	private void ManageHandObject()
+	{
+		/* 検出されたとき	*/
+		if(detectedHandPositions.Count > 0)
+		{
+			/* すでにHandObjectを生成しているとき	*/
+			if (handObjects.Count > 0)
+			{
+				UpdateHandObject();// 移動
+				AddHandObject();// 追加
+			}
+			/* まだHandObjectを生成してないとき(=新たに検出された)	*/
+			else
+			{
+				AddHandObject();
+			}
+		}
+		/* 何も検出されなかったとき	*/
+		else
+		{
+			/* すでにHandObjectを生成しているとき(=消えた)	*/
+			if (handObjects.Count > 0)
+			{
+				AllDeleteHandObject();// 削除
+			}
+			/* まだHandObjectを生成してないとき	*/
+			else
+			{
+				// 何もしなくていい
+			}
+		}
+	}
+
+	/*
+	 * 座標更新処理
+	 */
+	private void UpdateHandObject()
+	{
+		/* 移動したHnadObjectの座標を更新する	*/
+		for (int i= handObjects.Count-1; i>=0; i--)
+		{
+			bool isMoved = false;// 移動したか
+
+			for (int j=detectedHandPositions.Count-1; j>=0; j--)
+			{
+				float deltaPosX = handObjects[i].transform.position.x - detectedHandPositions[j].x;
+				float deltaPosY = handObjects[i].transform.position.y - detectedHandPositions[j].y;
+
+				/* 移動したなら	*/
+				if ((Mathf.Abs(deltaPosX) < ERROR_MARGIN) && (Mathf.Abs(deltaPosY) < ERROR_MARGIN))
+				{
+					/* 座標を更新する	*/
+					handObjects[i].GetComponent<Rigidbody>().MovePosition(detectedHandPositions[j]);
+					detectedHandPositions.RemoveAt(j);
+
+					isMoved = true;
+					break;
+				}
+			}
+
+			/* 移動したと判定できないなら(=消えた)	*/
+			if(!isMoved)
+			{
+				Destroy(handObjects[i]);
+				handObjects.RemoveAt(i);
+			}
+		}
+	}
+
+	/*
+	 * HandObject追加処理
+	 */
+	private void AddHandObject()
+	{
+		/* 検出された数だけ追加する	*/
+		for (int i=detectedHandPositions.Count-1; i>=0; i--)
+		{
+			handObjects.Add(CreateHandObject(detectedHandPositions[i]));
+			detectedHandPositions.RemoveAt(i);
+		}
+	}
+
+	/*
+	 * 削除(何も検出されなかったとき)
+	 */
+	private void AllDeleteHandObject()
+	{
+		for(int i=handObjects.Count-1; i>=0; i--)
+		{
+			Destroy(handObjects[i]);
+			handObjects.RemoveAt(i);
+		}
+	}
+
+	/*
+	 * HandObject生成処理
+	 */
+	private GameObject CreateHandObject(Vector3 createPos)
+	{
+		return Instantiate(handObjectPrefab, createPos, Quaternion.identity);
+	}
+}
---
+++ CameraMusicAppSample/CameraMusic/Assets/WebCamController1.cs
@@ -0,0 +1,111 @@
+﻿namespace OpenCvSharp
+{
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	using OpenCvSharp.Tracking;
+
+	public class WebCamController1 : MonoBehaviour
+	{
+		private int width = 1920;
+		private int height = 1080;
+		private int fps = 60;
+		private Texture2D cap_tex;
+		private Texture2D out_tex;
+		private WebCamTexture webCamTexture;
+		private Color32[] colors = null;
+
+		private Tracker tracker;
+		private bool isCaribrated;
+		private bool isInitedTracking;
+
+		/* HSVのパラメータは(色相、彩度、明度)	*/
+		private readonly static Scalar SKIN_LOWER = new Scalar(130, 10, 60);//(0, 30, 60);
+		private readonly static Scalar SKIN_UPPER = new Scalar(255, 50, 160);//(10, 160, 240);
+
+		IEnumerator Init()
+		{
+			while (true)
+			{
+				if (webCamTexture.width > 16 && webCamTexture.height > 16)
+				{
+					colors = new Color32[webCamTexture.width * webCamTexture.height];
+					cap_tex = new Texture2D(webCamTexture.width, webCamTexture.height, TextureFormat.RGBA32, false);
+					break;
+				}
+				yield return null;
+			}
+		}
+
+		/*
+		 * 初期化処理
+		 */
+		void Start()
+		{
+			/* もろもろ初期化	*/
+			WebCamDevice[] devices = WebCamTexture.devices;
+			webCamTexture = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
+			webCamTexture.Play();
+
+			StartCoroutine(Init());
+
+			tracker = Tracker.Create(TrackerTypes.MIL);
+			isCaribrated = false;
+			isInitedTracking = false;
+		}
+
+		/*
+		 * 周期処理
+		 */
+		void Update()
+		{
+			if (colors != null)
+			{
+				webCamTexture.GetPixels32(colors);
+
+				int width = webCamTexture.width;
+				int height = webCamTexture.height;
+
+				cap_tex.SetPixels32(this.colors);
+				cap_tex.Apply();
+
+				Mat outMat = Unity.TextureToMat(cap_tex);
+				Rect2d obj = new Rect2d(new Point2d((outMat.Width / 2) - 400, 200), new Size2d(200, 200));
+				/* キャリブレーション用の処理	*/
+				if (!isInitedTracking)
+				{
+					Cv2.Rectangle(outMat, new Rect((outMat.Width / 2) - 400, 200, 200, 200), new Scalar(0, 0, 255), 5);// キャリブレーション用の矩形を表示
+					Cv2.Rectangle(outMat, new Rect((outMat.Width / 2) + 400, 200, 200, 200), new Scalar(0, 0, 255), 5);// キャリブレーション用の矩形を表示
+					// キャリブレーション判定
+				}
+
+				/* トラッキング開始(指定領域に肌色がたくさんある状態で一定時間経ったら)	*/
+				if (isCaribrated)
+				{
+					tracker.Init(Unity.TextureToMat(cap_tex), obj);
+					isInitedTracking = true;
+				}
+				/* トラッキング中	*/
+				if(isCaribrated && isInitedTracking)
+				{
+					tracker.Update(Unity.TextureToMat(cap_tex), ref obj) ;
+				}
+
+				out_tex = Unity.MatToTexture(outMat);
+			}
+			
+			GetComponent<RawImage>().texture = out_tex;
+		}
+
+		/*
+		 * ボタン押下時の処理
+		 */
+		public void OnPushButton()
+		{
+			isCaribrated = true;
+		}
+	}
+}
